f3d7750c05752e5ab418a4d6a05a20be3e4c84a3
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2016-06-12 14:39:49 -0500

    Add files via upload

--- connector/mysql-connector-to-schema-eecs_499.php
+++ connector/mysql-connector-to-schema-eecs_499.php
@@ -1,1 +1,32 @@
-mysql-connector-to-schema-eecs_499.php
+<!-- mysql-connector-to-schema-eecs_499.php -->
+<!-- http://php.net/manual/en/book.mysqli.php -->
+<!-- @author = "Yuanhui Yang" -->
+<!-- @email = "yuanhui.yang@u.northwestern.edu" -->
+
+<?php
+
+$input = "SELECT * FROM GRE_Query WHERE Gre_verbal > 120 AND Gre_quan > 130";
+
+$mysqli = new mysqli("localhost", "root", "root", "eecs_499", 3306);
+
+/* check connection */
+if ($mysqli->connect_errno) {
+    echo "Connect failed: " . $mysqli->connect_error . "\n";
+    exit();
+}
+
+/* Create table doesn't return a resultset */
+if ($mysqli->query($input) === TRUE) {
+    echo "Table GRE_Query successfully created.\n";
+}
+
+/* Select queries return a resultset */
+if ($output = $mysqli->query($input)) {
+    echo "Select returned rows." . $output->num_rows . "\n";
+
+    /* free result set */
+    $output->close();
+}
+
+$mysqli->close();
+?>
