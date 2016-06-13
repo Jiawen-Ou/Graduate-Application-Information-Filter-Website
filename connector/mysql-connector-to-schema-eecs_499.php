c78b0455bcbe2e247625ec723d779809218caa50
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2016-06-13 05:53:46 -0500

    Update mysql-connector-to-schema-eecs_499.php

--- connector/mysql-connector-to-schema-eecs_499.php
+++ connector/mysql-connector-to-schema-eecs_499.php
@@ -2,8 +2,10 @@
 <!-- http://php.net/manual/en/book.mysqli.php -->
 <!-- @author = "Yuanhui Yang" -->
 <!-- @email = "yuanhui.yang@u.northwestern.edu" -->
+
 <?php
 $ping = $_GET["ping"];
+$method = $_GET["method"];
 // Hostname: localhost, username: root, password: root, db: eecs_499
 $mysqli = new mysqli('localhost', 'root', 'root', 'eecs_499');
 	if ($mysqli->connect_errno) {
@@ -14,10 +16,7 @@ $mysqli = new mysqli('localhost', 'root', 'root', 'eecs_499');
 	exit;
 }
 
-// $ping = "SELECT EmployID, Program, Gender, Ethnicity, Country FROM GRE_Query WHERE ( ( Gre_verbal > 100 ) )";
-// echo $ping;
-
-if (!$output = $mysqli->query($ping)) {
+if (!$pong = $mysqli->query($ping)) {
 	echo "Sorry, the website is experiencing problems.";
 	echo "Error: Our query failed to execute and here is why: \n";
 	echo "Query: " . $ping . "\n";
@@ -26,22 +25,21 @@ if (!$output = $mysqli->query($ping)) {
 	exit;
 }
 
-if ($output->num_rows === 0) {
-	echo "We could not find a match for ID $aid, sorry about that. Please try again.";
+if ($pong->num_rows === 0) {
+	echo "We could not find a match, sorry about that. Please try again.";
 	exit;
 }
-echo "<table>";
-while ($element = $output->fetch_assoc()) {
-	echo "<tr>";
-	echo "<td>" . $element["EmployID"] . "</td>";
-	echo "<td>" . $element["Program"] . "</td>";
-	echo "<td>" . $element["Gender"] . "</td>";
-	echo "<td>" . $element["Ethnicity"] . "</td>";
-	echo "<td>" . $element["Country"] . "</td>";
-	echo "</tr>";
+if ($method === "Search") {
+	while ($row = $pong->fetch_array(MYSQLI_NUM)) {
+		echo "<tr>";
+		for ($column = 0; $column < $pong->field_count; $column++) {
+			echo "<td>";
+			echo $row[$column];
+			echo "</td>";
+		}
+		echo "</tr>";
+	}
 }
-echo "</table>";
-
-$output->free();
+$pong->free();
 $mysqli->close();
 ?>
