6833dd5adc534522f0c5671d8f01a35667086e12
Yuanhui Yang <yuanhui-yang@users.noreply.github.com>
2016-06-12 19:53:50 -0500

    Add files via upload

--- connector/mysql-connector-to-schema-eecs_499.php
+++ connector/mysql-connector-to-schema-eecs_499.php
@@ -2,11 +2,8 @@
 <!-- http://php.net/manual/en/book.mysqli.php -->
 <!-- @author = "Yuanhui Yang" -->
 <!-- @email = "yuanhui.yang@u.northwestern.edu" -->
-<!DOCTYPE html>
-<html>
-<body>
 <?php
-// $input = $_REQUEST["q"];
+$ping = $_GET["ping"];
 // Hostname: localhost, username: root, password: root, db: eecs_499
 $mysqli = new mysqli('localhost', 'root', 'root', 'eecs_499');
 	if ($mysqli->connect_errno) {
@@ -17,13 +14,13 @@ $mysqli = new mysqli('localhost', 'root', 'root', 'eecs_499');
 	exit;
 }
 
-$input = "SELECT EmployID, Program, Gender, Ethnicity, Country FROM GRE_Query WHERE ( ( Gre_verbal > 100 ) )";
-// echo $input;
+// $ping = "SELECT EmployID, Program, Gender, Ethnicity, Country FROM GRE_Query WHERE ( ( Gre_verbal > 100 ) )";
+// echo $ping;
 
-if (!$output = $mysqli->query($input)) {
+if (!$output = $mysqli->query($ping)) {
 	echo "Sorry, the website is experiencing problems.";
 	echo "Error: Our query failed to execute and here is why: \n";
-	echo "Query: " . $input . "\n";
+	echo "Query: " . $ping . "\n";
 	echo "Errno: " . $mysqli->errno . "\n";
 	echo "Error: " . $mysqli->error . "\n";
 	exit;
@@ -37,6 +34,10 @@ echo "<table>";
 while ($element = $output->fetch_assoc()) {
 	echo "<tr>";
 	echo "<td>" . $element["EmployID"] . "</td>";
+	echo "<td>" . $element["Program"] . "</td>";
+	echo "<td>" . $element["Gender"] . "</td>";
+	echo "<td>" . $element["Ethnicity"] . "</td>";
+	echo "<td>" . $element["Country"] . "</td>";
 	echo "</tr>";
 }
 echo "</table>";
@@ -44,5 +45,3 @@ echo "</table>";
 $output->free();
 $mysqli->close();
 ?>
-</body>
-</html>
